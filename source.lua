ba939a847d2d351a0b4d62e4a855e1645bafb123
someoneus <wilsonjunor7@gmail.com>
2025-03-12 16:17:37 +0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,21 +1,446 @@
-function easy:CreateWindow(name, x, y)
-gui = Instance.new("ScreenGui")
-  gui.Parent = game:GetService("CoreGui")
-  window = Instance.new("Frame")
-  window.Parent = gui
-  window.Size = UDim2.new(x, 0, y, 0)
-  window.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-  local ui = Instance.new("UIStroke")
-  ui.Parent = window
-  ui.Thickness = 3
-  ui.Transparency = 0.35
-  ui.Color = Color3.fromRGB(200, 0, 0)
-  local title = Instance.new("TextLabel")
-  title.Parent = window
-  title.Size = UDim2.new(0.6, 0, 0.1, 0)
-  title.TextColor3 = Color3.fromRGB(255, 255, 255)
-  title.BackgroundTransparency = 1
-  title.Text = name
-  title.TextScaled = true
-  title.Font = Enum.Font.Code
+
+
+task.spawn(function()
+    warn("EasyLibrary Is Booting")
+    end)
+    function easyCreateWindow(name, x, y)
+        local name = name or "EasyLibrary"
+        local core = game:GetService("CoreGui")
+        local dupe = core:FindFirstChild("EasyLib: "..name)
+        if dupe then
+            warn("Duplicated Instances ["..name.."]")
+            dupe:Destroy()
+        end
+    
+        gui = Instance.new("ScreenGui")
+        gui.Parent = game:GetService("CoreGui")
+        gui.ResetOnSpawn = false
+        gui.Name = "EasyLib: "..name
+        window = Instance.new("Frame")
+        window.Parent = gui
+        window.Size = UDim2.new(x, 0, y, 0)
+        window.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+        window.BackgroundTransparency = 0.1
+        local hide = Instance.new("TextButton")
+        hide.Parent = gui
+        hide.Size = UDim2.new(0.1, 0, 0.1, 0)
+        hide.Position = UDim2.new(0.45, 0, 0, 0)
+        hide.Text = name.." +"
+        hide.TextScaled = true
+        hide.BackgroundTransparency = 1
+        hide.Visible = false
+        hide.Font = Enum.Font.GothamBold
+
+task.spawn(function()
+    local hue = 0
+    while true do
+        hue = (hue + 0.005) % 1
+        hide.TextColor3 = Color3.fromHSV(hue, 1, 1)
+        task.wait(0.05) 
+    end
+end)
+
+        local tx = Instance.new("TextLabel")
+        tx.Parent = window
+        tx.Size = UDim2.new(0.95, 0, 0.1, 0)
+        tx.Position = UDim2.new(0.025, 0, 0.005, 0)
+        tx.TextScaled = true
+        tx.Font = Enum.Font.Code
+        tx.TextXAlignment = Enum.TextXAlignment.Left
+        tx.Text = name
+        tx.BackgroundTransparency = 1
+        tx.TextColor3 = Color3.fromRGB(255, 255, 255)
+        local st = Instance.new("UIStroke")
+        st.Parent = window
+        st.Thickness = 3
+        st.Color = Color3.fromRGB(200, 0, 0)
+        st.Transparency = 0.25
+        bar = Instance.new("ScrollingFrame")
+        bar.Parent = window
+        bar.Size = UDim2.new(0.95, 0, 0.15, 0)
+        bar.Position = UDim2.new(0.025, 0, 0.125, 0)
+        bar.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+        bar.ScrollBarImageTransparency = 1
+        bar.BackgroundTransparency = 1
+        local x = Instance.new("TextButton")
+        x.Parent = window
+        x.TextScaled = true
+        x.Font = Enum.Font.GothamBold
+        x.Text = "X"
+        x.Size = UDim2.new(0.1, 0, 0.1, 0)
+        x.Position = UDim2.new(0.9, 0, -0.1, 0)
+        x.BackgroundTransparency = 1
+        local z = Instance.new("TextButton")
+        z.Parent = window
+        z.TextScaled = true
+        z.Font = Enum.Font.GothamBold
+        z.Text = "-"
+        z.Size = UDim2.new(0.1, 0, 0.1, 0)
+        z.Position = UDim2.new(0.8, 0, -0.1, 0)
+        z.BackgroundTransparency = 1
+        local ui = Instance.new("UIListLayout")
+        ui.FillDirection = Enum.FillDirection.Horizontal
+        ui.Parent = bar
+        ui.Padding = UDim.new(0.05, 0)
+        ui.SortOrder = Enum.SortOrder.LayoutOrder
+        ui.VerticalAlignment = Enum.VerticalAlignment.Top
+        bar.AutomaticCanvasSize = Enum.AutomaticSize.X
+        bar.CanvasSize = UDim2.new(2, 0, 0, 0)
+            local UIS = game:GetService("UserInputService")
+x.MouseButton1Click:Connect(function()
+warn("Easy Interface ["..name.."] has been killed")
+window:Destroy()
+end)
+x.TouchTap:Connect(function()
+warn("Easy Interface ["..name.."] has been killed")
+window:Destroy()
+end)
+z.MouseButton1Click:Connect(function()
+hide.Visible = true
+window.Visible = false
+end)
+z.TouchTap:Connect(function()
+hide.Visible = true
+window.Visible = false
+end)
+hide.MouseButton1Click:Connect(function()
+hide.Visible = false
+window.Visible = true
+end)
+hide.TouchTap:Connect(function()
+hide.Visible = false
+window.Visible = true
+end)
+local frame = window
+
+local dragging
+local dragInput
+local startPos
+local dragStart
+
+local function update(input)
+    local delta = input.Position - dragStart
+    frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
 end
+
+frame.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+        dragging = true
+        dragStart = input.Position
+        startPos = frame.Position
+        
+        input.Changed:Connect(function()
+            if input.UserInputState == Enum.UserInputState.End then
+                dragging = false
+            end
+        end)
+    end
+end)
+
+frame.InputChanged:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseMovement then
+        dragInput = input
+    end
+end)
+
+UIS.InputChanged:Connect(function(input)
+    if input == dragInput and dragging then
+        update(input)
+    end
+end)
+
+local frame = hide
+
+local dragging
+local dragInput
+local startPos
+local dragStart
+
+local function update(input)
+    local delta = input.Position - dragStart
+    frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+end
+
+frame.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+        dragging = true
+        dragStart = input.Position
+        startPos = frame.Position
+        
+        input.Changed:Connect(function()
+            if input.UserInputState == Enum.UserInputState.End then
+                dragging = false
+            end
+        end)
+    end
+end)
+
+frame.InputChanged:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseMovement then
+        dragInput = input
+    end
+end)
+
+UIS.InputChanged:Connect(function(input)
+    if input == dragInput and dragging then
+        update(input)
+    end
+end)
+
+    end
+
+local lastCreatedFrame = nil
+
+function easyCreateTab(name)
+    local tab = Instance.new("TextButton")
+    tab.Text = name or "Tab"
+    tab.Parent = bar
+    tab.Size = UDim2.new(0.2, 0, 0.8, 0)
+    tab.TextColor3 = Color3.fromRGB(255, 255, 255)
+    tab.BackgroundColor3 = Color3.fromRGB(70, 70, 70)
+    tab.Font = Enum.Font.Code
+    tab.TextScaled = true
+    local frame = Instance.new("ScrollingFrame")
+    frame.Visible = false
+    frame.Parent = window
+    frame.Position = UDim2.new(0.025, 0, 0.3, 0)
+    frame.Size = UDim2.new(0.95, 0, 0.65, 0)
+    frame.Name = "tabFrame_" .. name
+    frame.BackgroundColor3 = Color3.fromRGB(70, 70, 70)
+    frame.BackgroundTransparency = 1
+    frame.AutomaticCanvasSize = Enum.AutomaticSize.Y
+    frame.CanvasSize = UDim2.new(0, 0, 0, 0)
+    frame.ScrollBarImageTransparency = 1
+    local corner = Instance.new("UICorner")
+    corner.CornerRadius = UDim.new(0.15, 0)
+    corner.Parent = tab
+    local uiListLayout = Instance.new("UIListLayout")
+    uiListLayout.Parent = frame
+    uiListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+    uiListLayout.Padding = UDim.new(0.01, 0)
+    
+    tab.MouseButton1Click:Connect(function()
+        for _, v in pairs(window:GetDescendants()) do
+            if v:IsA("ScrollingFrame") and v.Name:find("tabFrame_") then
+                v.Visible = false
+            end
+        end
+        frame.Visible = true
+    end)
+    
+    tab.TouchTap:Connect(function()
+        for _, v in pairs(window:GetDescendants()) do
+            if v:IsA("ScrollingFrame") and v.Name:find("tabFrame_") then
+                v.Visible = false
+            end
+        end
+        frame.Visible = true
+    end)
+    
+    lastCreatedFrame = frame
+end
+
+function easyCreateLabel(text)
+    if lastCreatedFrame then
+        local label = Instance.new("TextLabel")
+        label.Parent = lastCreatedFrame
+        label.Size = UDim2.new(1, 0, 0.15, 0)
+        label.Text = text
+        label.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+        label.Font = Enum.Font.Cartoon
+        label.TextColor3 = Color3.fromRGB(255, 255, 255)
+        label.TextScaled = true
+        label.TextXAlignment = Enum.TextXAlignment.Left
+        label.ZIndex = 2
+        local corner = Instance.new("UICorner")
+        corner.CornerRadius = UDim.new(0.1, 0)
+        corner.Parent = label
+        lastCreatedLabel = label
+    end
+end
+
+function easyCreateButton(options)
+    if lastCreatedFrame then
+        local button = Instance.new("TextButton")
+        button.Parent = lastCreatedFrame
+        button.Size = UDim2.new(1, 0, 0.15, 0)
+        button.Text = options.Name
+        button.TextScaled = true
+        button.TextColor3 = Color3.fromRGB(255, 255, 255)
+        button.Font = Enum.Font.Cartoon
+        button.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+        button.ZIndex = 2
+        button.TextXAlignment = Enum.TextXAlignment.Left
+        local corner = Instance.new("UICorner")
+        corner.CornerRadius = UDim.new(0.1, 0)
+        corner.Parent = button
+        local tween = game:GetService("TweenService")
+        local info = TweenInfo.new(
+            0.25,
+            Enum.EasingStyle.Quad,
+            Enum.EasingDirection.InOut,
+            0,
+            false
+        )
+        local goal = {
+            BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+        }
+        local anim = tween:Create(button, info, goal)
+
+        button.MouseButton1Click:Connect(function()
+        if options and options.Callback then
+            options.Callback()
+            button.BackgroundColor3 = Color3.fromRGB(125, 125, 125)
+            anim:Play()
+        end
+    end)
+    button.TouchTap:Connect(function()
+        if options and options.Callback then
+            options.Callback()
+            button.BackgroundColor3 = Color3.fromRGB(120, 120, 120)
+            anim:Play()
+        end
+    end)
+end
+end
+
+function easyUpdateLabel(msg)
+    if lastCreatedLabel then
+        local label = lastCreatedLabel
+        label.Text = msg
+    end
+end
+
+function easyCreateInput(txtoption)
+    if txtoption then
+        local frame = Instance.new("Frame")
+        frame.Parent = lastCreatedFrame
+        frame.Size = UDim2.new(1, 0, 0.15, 0)
+        frame.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+        local ui = Instance.new("UICorner")
+        ui.Parent = frame
+        ui.CornerRadius = UDim.new(0.1, 0)
+        local text = Instance.new("TextLabel")
+        text.Text = txtoption.Name
+        text.TextScaled = true
+        text.TextXAlignment = Enum.TextXAlignment.Left
+        text.TextColor3 = Color3.fromRGB(255, 255, 255)
+        text.Font = Enum.Font.Cartoon
+        text.Parent = frame
+        text.Size = UDim2.new(0.5, 0, 1, 0)
+        text.BackgroundTransparency = 1
+        local textb = Instance.new("TextBox")
+        textb.Parent = frame
+        textb.TextScaled = true
+        textb.Position = UDim2.new(0.525, 0, 0, 0)
+        textb.Size = UDim2.new(0.475, 0, 1, 0)
+        textb.Text = txtoption.Placeholder
+        textb.PlaceholderText = txtoption.Placeholder
+        textb.PlaceholderColor3 = Color3.fromRGB(90, 90, 90)
+        textb.TextColor3 = Color3.fromRGB(255, 255, 255)
+        textb.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+        local ui = Instance.new("UICorner")
+        ui.Parent = textb
+        ui.CornerRadius = UDim.new(0.1, 0)
+        textb.FocusLost:Connect(function()
+        if txtoption.Callback then
+            txtoption.Callback(textb.Text)
+            end
+        end)
+    end
+end
+
+function easyCreateToggle(option)
+    local frame = Instance.new("Frame")
+    frame.Parent = lastCreatedFrame
+    frame.Size = UDim2.new(1, 0, 0.15, 0)
+    frame.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+    local ui = Instance.new("UICorner")
+    ui.Parent = frame
+    ui.CornerRadius = UDim.new(0.1, 0)
+    local text = Instance.new("TextLabel")
+    text.Text = option.Name
+    text.TextScaled = true
+    text.TextColor3 = Color3.fromRGB(255, 255, 255)
+    text.Font = Enum.Font.Cartoon
+    text.Parent = frame
+    text.Size = UDim2.new(0.5, 0, 1, 0)
+    text.BackgroundTransparency = 1
+    text.TextXAlignment = Enum.TextXAlignment.Left
+    local out = Instance.new("Frame")
+    out.Parent = frame
+    out.Size = UDim2.new(0.15, 0, 0.5, 0)
+    out.Position = UDim2.new(0.8, 0, 0.25, 0)
+    out.BackgroundColor3 = Color3.fromRGB(90, 90, 90)
+    out.BackgroundTransparency = 1
+    local ui = Instance.new("UIStroke")
+    ui.Parent = out
+    ui.Thickness = 3.5
+    ui.Color = Color3.fromRGB(90, 90, 90)
+    ui.Transparency = 0
+    local ui = Instance.new("UICorner")
+    ui.Parent = out
+    ui.CornerRadius = UDim.new(0.5, 0)
+    local toggle = Instance.new("TextButton")
+    toggle.Parent = out
+    toggle.Text = ""
+    toggle.Size = UDim2.new(0.325, 0, 0.85, 0)
+    toggle.Position = UDim2.new(0.025, 0, 0.09, 0)
+    toggle.BackgroundColor3 = Color3.fromRGB(90, 90, 90)
+    local ui = Instance.new("UICorner")
+    ui.Parent = toggle
+    ui.CornerRadius = UDim.new(1, 0)
+    local tween = game:GetService("TweenService")
+    local info = TweenInfo.new(
+        0.1,
+        Enum.EasingStyle.Quad,
+        Enum.EasingDirection.InOut,
+        0,
+        false
+    )
+    local goal = {
+        Position = UDim2.new(0.65, 0, 0.09, 0),
+        BackgroundColor3 = Color3.fromRGB(200, 200, 200)
+    }
+    local anim = tween:Create(toggle, info, goal)
+
+        local info2 = TweenInfo.new(
+        0.1,
+        Enum.EasingStyle.Quad,
+        Enum.EasingDirection.InOut,
+        0,
+        false
+    )
+    local goal2 = {
+        Position = UDim2.new(0.025, 0, 0.09, 0),
+        BackgroundColor3 = Color3.fromRGB(90, 90, 90)
+    }
+    local anim2 = tween:Create(toggle, info2, goal2)
+    local Value = false
+    toggle.MouseButton1Click:Connect(function()
+    Value = not Value
+    if Value then
+    option.Callback(Value)
+    anim:Play()
+    else
+    option.Callback(Value)
+    anim2:Play()
+    end
+    end)
+    toggle.TouchTap:Connect(function()
+    Value = not Value
+    if Value then
+    option.Callback(Value)
+    anim:Play()
+    else
+    option.Callback(Value)
+    anim2:Play()
+    end
+    end)
+end
+
+--[[
+
+# Thank you
+
+]]
